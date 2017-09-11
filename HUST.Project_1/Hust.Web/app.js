bcbf86b0a896dbd5274209f1f76f30f99e238812
TruongTienDuc <ductt.it.hust@gmail.com>
2017-09-12 01:44:10 +0700

    update web

---
+++ HUST.Project_1/Hust.Web/app.js
@@ -0,0 +1,6 @@
+﻿var app = angular.module('HUST_PROJECT_1', ['apiHelper']);
+
+app.run(["$rootScope", '$state', '$window', '$location', '$timeout', function ($rootScope, $state, $window, $location, $timeout) {
+  
+
+}]);
