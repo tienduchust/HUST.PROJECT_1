6299f4a6ecad2d3aa5d52381a1c1d1ab58fde479
KhoiNguyenICT <khoinguyenict>
2017-09-13 01:53:21 -0700

    Remove AppHust And Add Hust.Web

--- HUST.Project_1/Hust.Web/e2e/app.e2e-spec.ts
+++ HUST.Project_1/Hust.Web/e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('apphust App', () => {
+describe('hust.web App', () => {
   let page: AppPage;
 
   beforeEach(() => {
--- HUST.Project_1/Hust.Web/src/app/app.module.ts
+++ HUST.Project_1/Hust.Web/src/app/app.module.ts
@@ -3,19 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { RegisterComponent } from './register/register.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    DashboardComponent,
     RegisterComponent,
-    SidebarComponent,
-    HeaderComponent
+    DashboardComponent,
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
