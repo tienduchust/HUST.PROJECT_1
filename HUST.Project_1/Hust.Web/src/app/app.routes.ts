021b47de3c4801ca58d4643c3035390308c9477d
TruongTienDuc <ductt.it.hust@gmail.com>
2017-09-16 18:29:08 +0700

    Delete web app

--- HUST.Project_1/Hust.Web/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from "@angular/router";
-
-export const appRoutes: Routes = [
-  // { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', loadChildren: './authenication/login/login.module#LoginModule' },
-  { path: 'register', loadChildren: './authenication/register/register.module#RegisterModule' }
-]
