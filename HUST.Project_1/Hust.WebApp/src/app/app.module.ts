00bfc229e42cb360d116b06feac5019800baf4cf
TruongTienDuc <ductt.it.hust@gmail.com>
2017-09-21 01:44:06 +0700

    update dashboard , header, sidebar

--- HUST.Project_1/Hust.WebApp/src/app/app.module.ts
+++ HUST.Project_1/Hust.WebApp/src/app/app.module.ts
@@ -2,14 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AuthenticationComponent } from './authentication/authentication.component';
-
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
     AppComponent,
-    AuthenticationComponent
+    AuthenticationComponent,
+    SidebarComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: 'hustDashBoard',
+        component: DashboardComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
