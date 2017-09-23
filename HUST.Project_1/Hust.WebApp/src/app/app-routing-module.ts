5da29f31dee499b0a8d8322703ff09585a9b8112
TruongTienDuc <ductt.it.hust@gmail.com>
2017-09-23 21:09:43 +0700

    update route

--- HUST.Project_1/Hust.WebApp/src/app/app-routing-module.ts
+++ HUST.Project_1/Hust.WebApp/src/app/app-routing-module.ts
@@ -2,19 +2,21 @@ import { EmailComponent } from './email/email.component';
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 
-import{AuthenticationComponent} from './authentication/authentication.component';
-import{DashboardComponent} from './dashboard/dashboard.component';
+import { AuthenticationComponent } from './authentication/authentication.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes:Routes=[
-    {path:'',redirectTo:'/login', pathMatch:'full'},
-    {path:'login', component:AuthenticationComponent},
-    {path:'dashboard', component:DashboardComponent, 
-    children:[
-        {path:'email', component: EmailComponent, outlet:"appemail"}
-    ]}    
+const routes: Routes = [
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: 'login', component: AuthenticationComponent },
+    {
+        path: 'dashboard', component: DashboardComponent,
+        children: [
+            { path: 'email', component: EmailComponent, outlet: 'appemail' }
+        ]
+    }
 ];
 @NgModule({
-imports:[RouterModule.forRoot(routes)],
-exports:[RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule{}
+export class AppRoutingModule { }
